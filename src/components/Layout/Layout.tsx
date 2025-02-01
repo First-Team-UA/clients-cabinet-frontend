a9bead3bba459b4a0d7e2fb0d28504fe2c1a0e47
Ostap Kryven <Ostapkriven13@gmail.com>
2025-02-02 00:01:52 +0200

    I did the initial work
    Added base file
    Added base style
    Added base library

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,52 @@
+import { Link, Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import "./Layout.css"; 
+import "../../language/i18n"
+
+const Layout = () => {
+  const { t, i18n } = useTranslation();
+
+  const handleLanguageChange = (event: { target: { value: string | undefined; }; }) => {
+    i18n.changeLanguage(event.target.value); 
+  };
+
+  return (
+    <div className="layout">
+        {/* in the future, a new component (sidebar) */}
+      <aside className="sidebar">
+        <h2 className="logo">VseTV</h2>
+        <nav>
+          <ul>
+            <li><Link to="/dashboard">{t("dashboard")}</Link></li>
+            <li><Link to="/profile">{t("profile")}</Link></li>
+            <li><Link to="/tv-channels">{t("tvChannels")}</Link></li>
+            <li><Link to="/contacts">{t("contacts")}</Link></li>
+            <li><Link to="/test">{t("test")}</Link></li>
+          </ul>
+        </nav>
+
+        <button className="logout-button">{t("logout")}</button>
+      </aside>
+
+      <div className="main-content">
+      {/* in the future, a new component (header) */}
+        <header className="header">
+
+          <h1>Назва компанії</h1> 
+          {/* component (language switcher) */}
+          <select className="language-switcher" onChange={handleLanguageChange} defaultValue={i18n.language}>
+          <option value="en">English</option>
+          <option value="uk">Українська</option>
+        </select>
+        {/* component (theme switcher) */}
+        <div>Switcher theme</div>
+        </header>
+        <main>
+          <Outlet /> 
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/Test.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <h2>Test Page</h2>;
+  };
+  
+  export default Test;
